ed0a5ef38261cd755b684a09f068de5b4f0212bb
Sfaret <sfameitteam@gmail.com>
2024-08-03 13:48:41 +0100

    Add files via upload

--- login-page/server.js
+++ login-page/server.js
@@ -1,27 +1,23 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const { join } = require("path");
+const morgan = require("morgan");
+const helmet = require("helmet");
 const app = express();
-const port = process.env.PORT || 3000;
 
-// Middleware
-app.use(bodyParser.json());
-app.use(express.static('public'));
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.static(join(__dirname, "public")));
 
-// Route to handle sign-up
-app.post('/signup', (req, res) => {
-    const { email, password } = req.body;
-
-    // Simple validation and logging (in a real app, save to a database)
-    if (email && password) {
-        console.log(`New user signed up: ${email}`);
-        res.status(200).json({ message: "Sign up successful" });
-    } else {
-        res.status(400).json({ message: "Invalid data" });
-    }
+app.get("/auth_config.json", (req, res) => {
+  res.sendFile(join(__dirname, "auth_config.json"));
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    console.log(`Server is running on https://www.Sfarettech.com.ng:${port}`);
+app.get("/*", (_, res) => {
+  res.sendFile(join(__dirname, "index.html"));
 });
+
+process.on("SIGINT", function() {
+  process.exit();
+});
+
+module.exports = app;
